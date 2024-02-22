d4844609e94b8b7ab0853ff9d79d1eef9ad9097e
William Cook <wjjcook11@gmail.com>
2024-02-21 16:57:28 -0800

    Create Spring Boot project files

---
+++ src/main/java/org/example/bowlingaveragetracker/BowlingAverageTrackerApplication.java
@@ -0,0 +1,13 @@
+package org.example.bowlingaveragetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BowlingAverageTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BowlingAverageTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/bowlingaveragetracker/BowlingAverageTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bowlingaveragetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BowlingAverageTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
