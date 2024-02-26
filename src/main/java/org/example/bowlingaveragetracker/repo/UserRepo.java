b890b21c912f5e06edb60675ed22bee7f81774b3
William Cook <wjjcook11@gmail.com>
2024-02-25 23:02:01 -0800

    Update UserRepo.java

--- src/main/java/org/example/bowlingaveragetracker/repo/UserRepo.java
+++ src/main/java/org/example/bowlingaveragetracker/repo/UserRepo.java
@@ -1,2 +1,7 @@
-package org.example.bowlingaveragetracker.repo;public interface UserRepo {
+package org.example.bowlingaveragetracker.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.example.bowlingaveragetracker.model.User;
+
+public interface UserRepo extends JpaRepository {
 }
