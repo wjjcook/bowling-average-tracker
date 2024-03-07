31ca56986699f254abf4692414a5077f0a73dcdb
William Cook <wjjcook11@gmail.com>
2024-03-06 19:38:31 -0800

    Create user_form.html for adding new users

--- src/main/java/org/example/bowlingaveragetracker/controller/UserController.java
+++ src/main/java/org/example/bowlingaveragetracker/controller/UserController.java
@@ -19,4 +19,10 @@ public class UserController {
         model.addAttribute("listUsers", listUsers);
         return "users";
     }
+
+    @GetMapping("/users/new")
+    public String showNewForm(Model model) {
+        model.addAttribute("user", new User());
+        return "user_form";
+    }
 }
