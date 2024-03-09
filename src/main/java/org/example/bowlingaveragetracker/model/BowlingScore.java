93caeb787cfffeedeb9cff37bb968625c092f011
William Cook <wjjcook11@gmail.com>
2024-03-08 16:43:30 -0800

    Create BowlingScore.java class with fields

---
+++ src/main/java/org/example/bowlingaveragetracker/model/BowlingScore.java
@@ -0,0 +1,56 @@
+package org.example.bowlingaveragetracker.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "bowling_scores")
+public class BowlingScore {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "date")
+    private LocalDate date;
+
+    @Column(name = "score")
+    private Integer score;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+}
--- src/main/java/org/example/bowlingaveragetracker/model/User.java
+++ src/main/java/org/example/bowlingaveragetracker/model/User.java
@@ -8,7 +8,7 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Integer id;
 
     @Column(nullable = false, unique = true, length = 45)
     private String name;
@@ -17,16 +17,11 @@ public class User {
 
     public User() {}
 
-    public User(int id, String name){
-        this.id = id;
-        this.name = name;
-    }
-
     public int getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
