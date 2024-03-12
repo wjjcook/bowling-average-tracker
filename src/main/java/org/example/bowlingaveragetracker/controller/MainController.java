a1de929516ca163ab2ba7cf49b5b130bae3f87b3
William Cook <wjjcook11@gmail.com>
2024-03-12 16:24:08 -0700

    Populate dropdown menu in homepage

--- src/main/java/org/example/bowlingaveragetracker/controller/MainController.java
+++ src/main/java/org/example/bowlingaveragetracker/controller/MainController.java
@@ -1,13 +1,24 @@
 package org.example.bowlingaveragetracker.controller;
 
+import org.example.bowlingaveragetracker.model.User;
+import org.example.bowlingaveragetracker.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class MainController {
 
+    @Autowired
+    private UserService userService;
     @GetMapping("")
-    public String showHomePage(){
+    public String showHomePage(Model model){
+        List<User> listUsers = userService.listAll();
+        model.addAttribute("listUsers", listUsers);
         return "index";
     }
+
 }
