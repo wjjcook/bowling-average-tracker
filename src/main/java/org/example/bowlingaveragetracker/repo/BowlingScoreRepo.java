637bc6e92329566302db632afb4807dbd03a910c
William Cook <wjjcook11@gmail.com>
2024-03-11 17:05:43 -0700

    Update BowlingScore repo and service

--- src/main/java/org/example/bowlingaveragetracker/repo/BowlingScoreRepo.java
+++ src/main/java/org/example/bowlingaveragetracker/repo/BowlingScoreRepo.java
@@ -1,7 +1,11 @@
 package org.example.bowlingaveragetracker.repo;
 
 import org.example.bowlingaveragetracker.model.BowlingScore;
+import org.example.bowlingaveragetracker.model.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface BowlingScoreRepo extends CrudRepository<BowlingScore, Integer> {
+    List<BowlingScore> findByUser(User user);
 }
--- src/main/java/org/example/bowlingaveragetracker/service/BowlingScoreService.java
+++ src/main/java/org/example/bowlingaveragetracker/service/BowlingScoreService.java
@@ -1,4 +1,23 @@
 package org.example.bowlingaveragetracker.service;
 
+import org.example.bowlingaveragetracker.model.BowlingScore;
+import org.example.bowlingaveragetracker.model.User;
+import org.example.bowlingaveragetracker.repo.BowlingScoreRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class BowlingScoreService {
+    @Autowired
+    private BowlingScoreRepo bowlingScoreRepo;
+
+    public List<BowlingScore> getScoresByUser(User user) {
+        return bowlingScoreRepo.findByUser(user);
+    }
+
+    public void saveBowlingScore(BowlingScore bowlingScore) {
+        bowlingScoreRepo.save(bowlingScore);
+    }
 }
