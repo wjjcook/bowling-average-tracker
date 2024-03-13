f63efd97a6115592ed10e44da3eb6e9e9e915713
William Cook <wjjcook11@gmail.com>
2024-03-12 17:35:55 -0700

    Create BowlingScoreRepoTests.java and implement testAddNew method

--- src/test/java/org/example/bowlingaveragetracker/BowlingScoreRepoTests.java
+++ src/test/java/org/example/bowlingaveragetracker/BowlingScoreRepoTests.java
@@ -1,2 +1,42 @@
-package org.example.bowlingaveragetracker;public class BowlingScoreRepoTests {
+package org.example.bowlingaveragetracker;
+
+import org.assertj.core.api.Assertions;
+import org.example.bowlingaveragetracker.model.BowlingScore;
+import org.example.bowlingaveragetracker.model.User;
+import org.example.bowlingaveragetracker.repo.BowlingScoreRepo;
+import org.example.bowlingaveragetracker.repo.UserRepo;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+
+import java.util.Optional;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback(false)
+public class BowlingScoreRepoTests {
+    @Autowired
+    private BowlingScoreRepo scoreRepo;
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @Test
+    public void testAddNew() {
+        Integer userId = 17;
+        Optional<User> optionalUser = userRepo.findById(userId);
+        User user = optionalUser.get();
+
+        Assertions.assertThat(user).isNotNull();
+        Assertions.assertThat(user.getId()).isGreaterThan(0);
+
+        BowlingScore score = new BowlingScore();
+        score.setUser(user);
+        BowlingScore savedScore = scoreRepo.save(score);
+
+        Assertions.assertThat(savedScore).isNotNull();
+        Assertions.assertThat(savedScore.getId()).isGreaterThan(0);
+    }
 }
